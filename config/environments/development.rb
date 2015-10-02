b08eff25e97c1f6b50a6f33a0b12febbe9d3b62f
Sajjad Murtaza (01770) <sajjad.murtaza@nxb.com.pk>
2015-10-02 21:00:16 +0500

    Change SMTP setting

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,8 +42,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
       address: 'smtp.gmail.com',
       port: 587,
-      user_name: 'sajjadmurtaza.nxb@gmail.com',
-      password: '012358_@@@',
+      user_name: 'testestest772@gmail.com',
+      password: 'strength:',
       authentication: 'plain',
       enable_starttls_auto: true
   }
